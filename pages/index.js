187915c2c95e64cc73827d9666ec5eb2c06c6f48
Gregorio Lucena <info-analista7@iancarina.com.ve>
2020-09-22 11:00:22 -0400

    Resolve conflict

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { usestate, useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useRouter } from "next/router"
 import Head from "next/head"
 import { colors } from "styles/theme"
